923533b9993c833cb7f838b135c7a1d9cac0482b
Vhugala-Mathivha <vhugalamathivha@gmail.com>
2025-07-02 02:29:55 +0200

    modified the html, improved js by adding animation and generating message

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ function generatePoem(event){
     let istrucInput = document.querySelector("#user-instruction");
     let apiKey = "3fbcbabc4aac00t31918odda5085df79";
     let prompt =`generate a beautiful poem about ${istrucInput.value}`;
-    let context = `you are a romantic poem expert and love to write and read short poems. Your goal is to generate a 4 line short poem in basic HTML. Make sure you write only the poem and nothing else, do not include anything else like "html" but the poem. make sure to follow the user instructions`;
+    let context = `you are a romantic poem expert and love to write and read short poems. Your goal is to generate a 4 line short poem using basic html style .please do not show the html sentence at the top and the beggining, and the text color should be black. Make sure you write only the poem and nothing else, do not include anything else like but the poem.make sure to follow the user instructions`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    axios.get(apiUrl).then(displayPoem);
+    let poElement = document.querySelector("#poem");
+    poElement.classList.remove("hidden");
+    poElement.innerHTML = `<div class="blink">Generating a poem about ${istrucInput.value}...</div>`;
 
-   
-    
+    axios.get(apiUrl).then(displayPoem);
+ 
 }
 
 let peomElement = document.querySelector("#form-gen");
